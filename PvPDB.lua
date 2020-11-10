32b5eee56d6fb364c6126ce4ffd2fe9aa625dd87
Kevin Houdebert <kevin.houdebert@gmail.com>
2020-11-10 01:35:54 +0100

    fix factions overriding each other

--- PvPDB.lua
+++ PvPDB.lua
@@ -7,13 +7,13 @@ ns.REGION = GetCVar("portal"):lower()
 ns.db = {}
 ns.debug = false
 
-local function BracketTooltip(name, realm, bracketId, bracketName)
-    if ns.db[realm][name][bracketId] ~= nil then
-        local won = ns.db[realm][name][bracketId]['sms'][1]
-        local lost = ns.db[realm][name][bracketId]['sms'][2]
+local function BracketTooltip(name, realm, faction, bracketId, bracketName)
+    if ns.db[realm][faction][name][bracketId] ~= nil then
+        local won = ns.db[realm][faction][name][bracketId]['sms'][1]
+        local lost = ns.db[realm][faction][name][bracketId]['sms'][2]
         local winrate = math.floor(won*100/(won+lost) * 100) / 100
         GameTooltip:AddDoubleLine(
-            format("Ranked %s: %s CR", bracketName, ns.db[realm][name][bracketId]['cr']),
+            format("Ranked %s: %s CR", bracketName, ns.db[realm][faction][name][bracketId]['cr']),
             format("%sW / %sL (%s%%)", won,	lost, winrate),
             1, 1, 1, 1, 1, 1)
     end
@@ -24,7 +24,7 @@ local function Mouseover_OnEvent(self, event, ...)
         local name, realm = UnitName("mouseover")
         realm = realm and realm ~= "" and realm or GetNormalizedRealmName()
         local faction, _ = UnitFactionGroup("mouseover")
-        
+
         if ns.debug then
             GameTooltip:AddLine(" ")
             GameTooltip:AddLine("Tooltip Info")
@@ -32,33 +32,27 @@ local function Mouseover_OnEvent(self, event, ...)
             GameTooltip:AddLine("Realm: "..realm, 1, 1, 1)
             GameTooltip:AddLine("Faction: "..faction, 1, 1, 1)
             GameTooltip:AddLine("Region: "..ns.REGION, 1, 1, 1)
-            GameTooltip:Show()
+            GameTooltip:AddLine("db="..(tostring(ns.db)), 1, 1, 1)
             if ns.db ~= nil then
-                GameTooltip:AddLine("db not nil", 1, 1, 1)
+                GameTooltip:AddLine("db["..realm.."]="..(tostring(ns.db[realm])), 1, 1, 1)
                 if ns.db[realm] ~= nil then
-                    GameTooltip:AddLine("db[realm] not nil", 1, 1, 1)
-                    if ns.db[realm][name] ~= nil then
-                        GameTooltip:AddLine("db[realm][name] not nil", 1, 1, 1)
-                    else
-                        GameTooltip:AddLine("db[realm][name] nil", 1, 1, 1)
+                    GameTooltip:AddLine("db["..realm.."]["..faction.."]="..(tostring(ns.db[realm][faction])), 1, 1, 1)
+                    if ns.db[realm][faction] ~= nil then
+                        GameTooltip:AddLine("db["..realm.."]["..faction.."]["..name.."]="..(tostring(ns.db[realm][faction][name])), 1, 1, 1)
                     end
-                else
-                    GameTooltip:AddLine("db[realm] nil", 1, 1, 1)
                 end
-            else
-                GameTooltip:AddLine("db nil", 1, 1, 1)
             end
             GameTooltip:Show()
         end
 
-        if ns.db ~= nil and ns.db[realm] ~= nil and ns.db[realm][name] ~= nil then
+        if ns.db ~= nil and ns.db[realm] ~= nil and ns.db[realm][faction] ~= nil and ns.db[realm][faction][name] ~= nil then
             GameTooltip:AddLine(" ")
             GameTooltip:AddLine("Score PvPDB")
-            local hl = ns.db[realm][name]['hl'] and ns.db[realm][name]['hl'] or "No Data"
+            local hl = ns.db[realm][faction][name]['hl'] and ns.db[realm][faction][name]['hl'] or "No Data"
             GameTooltip:AddLine("Honor Level: "..hl, 1, 1, 1)
-            BracketTooltip(name, realm, "2v2", "2v2")
-            BracketTooltip(name, realm, "3v3", "3v3")
-            BracketTooltip(name, realm, "bg", "RBG")
+            BracketTooltip(name, realm, faction, "2v2", "2v2")
+            BracketTooltip(name, realm, faction, "3v3", "3v3")
+            BracketTooltip(name, realm, faction, "bg", "RBG")
             GameTooltip:Show()
         end
     end
